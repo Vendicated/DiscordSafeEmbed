f53157aceabccc428fd68066b5ec1fb3ffd86d98
Vendicated <vendicated@riseup.net>
2021-04-18 03:00:22 +0200

    add JSON endpoints and cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import { nanoid } from "nanoid";
 // @ts-ignore
-import { port } from "../config.json";
 import { existsEmbed, getEmbedById, saveEmbed } from "./manager";
 import { EmbedParseError, generateHtml, getFullUrl, parseEmbed } from "./util";
 
+const { BACKEND_PORT } = process.env;
+if (!BACKEND_PORT) throw new Error("BACKEND_PORT environment variable must be set.");
+
 const app = express();
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(
     express.json({
@@ -33,11 +41,19 @@ app.post("/embed/create", async (req, res) => {
     let id;
     do {
         id = nanoid(10);
-    } while (await existsEmbed(id));
+    } while (await existsEmbed(id)); // Race condition go brrrrr but its okay this is less likely than winning the lottery
 
     await saveEmbed(id, embed);
 
     res.status(200).end(JSON.stringify({ code: 200, url: `${getFullUrl(req)}/embed/${id}`, id }));
+});
+
+app.get("/embed/:id.json", async (req, res) => {
+    const { id } = req.params;
+    const embed = await getEmbedById(id);
+    if (!embed) return res.status(404).send(`Embed with id ${id} was not found`);
+
+    res.status(200).send(JSON.stringify(embed));
 });
 
 app.get("/embed/:id", async (req, res) => {
@@ -56,6 +72,23 @@ app.get("/embed", (req, res) => {
             embed.description = "Just head over to "; // TODO
         }
         res.status(200).send(generateHtml(embed, req));
+    } catch (error) {
+        if (error instanceof EmbedParseError) {
+            return res.status(400).end(error.message);
+        }
+        res.status(500).end("Internal Server Error");
+        throw error;
+    }
+});
+
+app.get("/embed.json", (req, res) => {
+    try {
+        const embed = parseEmbed(req.query as Record<string, string>);
+        if (!embed.title && !embed.description) {
+            embed.title = "Send embeds safely";
+            embed.description = "Just head over to "; // TODO
+        }
+        res.status(200).send(JSON.stringify(embed));
     } catch (error) {
         if (error instanceof EmbedParseError) {
             return res.status(400).end(error.message);
@@ -90,4 +123,4 @@ app.get("/oembed", (req, res) => {
     }
 });
 
-app.listen(port, () => console.info(`Listening on port ${port}`));
+app.listen(BACKEND_PORT, () => console.info(`Listening on port ${BACKEND_PORT}`));
